1b2f7bb40b6fc43fe0da3eb0697445af6240f20e
MarilynPapadopoulos <marilyn.volk@gmail.com>
2021-05-22 17:52:19 -0400

    created models and routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,37 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    attributes: [
+      'id',
+      'product_name',
+      'price'
+    ],
+  })
+  .then(dbProductData => res.json(dbProductData))
+  .catch(err => {
+
+  console.log(err);
+  res.status(500).json(err);
+  })
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbproductData => res.json(dbproductData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+    })
+  
+ 
 });
 
 // create new product
@@ -41,10 +66,11 @@ router.post('/', (req, res) => {
       res.status(200).json(product);
     })
     .then((productTagIds) => res.status(200).json(productTagIds))
-    .catch((err) => {
+    .catch(err => {
+
       console.log(err);
-      res.status(400).json(err);
-    });
+      res.status(500).json(err);
+      })
 });
 
 // update product
